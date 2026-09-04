1110709620380e00960ade8b2c8e3960ffa258c9
melnikartem4849-maker <melnikartem4849@gmail.com>
2026-09-04 22:45:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1100,5 +1100,8 @@ async def main():
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
